98ce79115fca39f24f2ad5001b8e76eab8b73605
NoTArZuZ <arbuztvshka@yandex.ru>
2024-06-21 16:36:43 +0500

    Fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,23 @@ if status is-interactive
     alias tree="eza --icons -aT"
     zoxide init fish | source
     starship init fish | source
+    # vim mode
+    function fish_user_key_bindings
+        # fish_default_key_bindings
+        fish_vi_key_bindings
+    end
+    # cursor shapes
+    set fish_cursor_default block
+    set fish_cursor_insert block
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    set fish_cursor_external block
+    set fish_cursor_visual block
+    # colors
     set fish_color_command green
     set fish_color_param magenta
     set fish_color_valid_path yellow --underline
+    # commands on start
     fastfetch
     # Commands to run in interactive sessions can go here
 end
