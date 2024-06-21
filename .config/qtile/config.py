250b5c74b355fc92e36548bf365f2cffe812d1ec
NoTArZuZ <arbuztvshka@yandex.ru>
2024-06-22 03:05:09 +0500

    Make bar transparency optional

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -254,7 +254,7 @@ screens = [
                 widget.Spacer(length = 8),
             ],
             24,
-            background = colors[0],
+            # background = colors[0], # Uncomment this line to enable bar transparency
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
